8d3a0730f3d01264ff2f90b79c197f04d2448c04
Vineet Rajpal <rajpalvineet164@gmail.com>
2025-08-30 13:27:03 +0530

    learned map and filter in js

---
+++ javascript/map.js
@@ -0,0 +1,21 @@
+// map , filter and arrows
+// without using map and filter doubling the array elements
+const input=[1,2,3,4,5];
+// const newArray=[];
+// for(let i=0;i<input.length;i++){
+//     newArray.push(input[i]*4);
+// }
+// console.log(newArray);
+
+// //using map function
+
+// const ans = input.map(function (i){
+//     return i*2;
+// });
+// console.log(ans);
+
+//filtering
+const arr=[1,2,3,4,5];
+for(let i=0;i<arr.size();i++){
+    new
+}
